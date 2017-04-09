1f1413441c11d677f5a92ce6a8f8f18462fbdfee
DarooriGopi <gpkrishna23@gmail.com>
2017-04-09 19:23:46 -0400

    restaurant with ionic

---
+++ Sweetberry/www/js/services.js
@@ -0,0 +1,66 @@
+'use strict';
+
+angular.module('conFusion.services', ['ngResource'])
+        .constant("baseURL","http://localhost:3000/")
+        .service('menuFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+    
+                this.getDishes = function(){
+                    
+                    return $resource(baseURL+"dishes/:id",null,  {'update':{method:'PUT' }});
+                    
+                };
+    
+                // implement a function named getPromotion
+                // that returns a selected promotion.
+                this.getPromotion = function() {
+                    return $resource(baseURL+"promotions/:id");
+                };
+    
+                        
+        }])
+            
+        
+        .factory('corporateFactory', ['$resource', 'baseURL', function($resource, baseURL) {
+          
+            var corpfac= {};
+            
+           corpfac.getLeaders= function(){
+                return $resource(baseURL + "leadership/:id");
+                };
+            return corpfac;
+        }])
+
+        .factory('feedbackFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+    
+    
+            return $resource(baseURL+"feedback/:id");
+    
+        }])
+
+        .factory('favoriteFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
+            var favFac = {};
+            var favorites = [];
+
+            favFac.addToFavorites = function (index) {
+                for (var i = 0; i < favorites.length; i++) {
+                    if (favorites[i].id == index)
+                        return;
+                }
+                favorites.push({id: index});
+            };
+            favFac.deleteFromFavorites = function (index) {
+                    for (var i = 0; i < favorites.length; i++) {
+                        if (favorites[i].id == index) {
+                            favorites.splice(i, 1);
+                        }
+                    }
+                }
+
+            favFac.getFavorites = function () {
+                        return favorites;
+                    };
+
+            return favFac;
+            }])
+
+;
